ba570a0920e6d8e248811088027f4a34b3db1671
Jefter Alves <jefteralveskf2@gmail.com>
2021-05-05 10:16:28 -0300

    Definição de tipos, importando biblioteca em JS e gerando a primeira aplicação.

--- app.ts
+++ app.ts
@@ -1,36 +1,14 @@
-class Spacecraft {
-    constructor( public propulsor: string ) {}
-    
-    jumpIntoHyperspace() {
-        console.log(`Entering hyperspace with ${this.propulsor}`) 
-    }
-}
+import {Spacecraft, Containership} from './base_ships'
+import {MillenniumFalcon} from './starfighters'
+import * as _ from 'lodash'
+
+console.log(_.pad("Typescript", 20, "*"))
 
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
-class MillenniumFalcon extends Spacecraft implements Containership {
-    cargoContainers: number
-
-    constructor() {
-        super('hyperdrive')
-        this.cargoContainers = 3
-    }
-
-    jumpIntoHyperspace() {
-        if( Math.random() >= 0.5 ) {
-            super.jumpIntoHyperspace()
-        }
-        else{ console.log('Failed to jump into hyperspace') }
-    }
-}
-
 let falcon = new MillenniumFalcon()
 falcon.jumpIntoHyperspace()
-
-interface Containership {
-    cargoContainers: number
-}
 
 let goodForTheJob = ( ship: Containership ) => ship.cargoContainers > 2
 console.log(`Is falcon good for the job? ${ goodForTheJob ( falcon ) ? 'YES': 'NO'}`)
--- dist/app.js
+++ dist/app.js
@@ -1,47 +1,12 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Spacecraft = /** @class */ (function () {
-    function Spacecraft(propulsor) {
-        this.propulsor = propulsor;
-    }
-    Spacecraft.prototype.jumpIntoHyperspace = function () {
-        console.log("Entering hyperspace with " + this.propulsor);
-    };
-    return Spacecraft;
-}());
-var ship = new Spacecraft('hyperdrive');
+"use strict";
+exports.__esModule = true;
+var base_ships_1 = require("./base_ships");
+var starfighters_1 = require("./starfighters");
+var _ = require("lodash");
+console.log(_.pad("Typescript", 20, "*"));
+var ship = new base_ships_1.Spacecraft('hyperdrive');
 ship.jumpIntoHyperspace();
-var MillenniumFalcon = /** @class */ (function (_super) {
-    __extends(MillenniumFalcon, _super);
-    function MillenniumFalcon() {
-        var _this = _super.call(this, 'hyperdrive') || this;
-        _this.cargoContainers = 3;
-        return _this;
-    }
-    MillenniumFalcon.prototype.jumpIntoHyperspace = function () {
-        if (Math.random() >= 0.5) {
-            _super.prototype.jumpIntoHyperspace.call(this);
-        }
-        else {
-            console.log('Failed to jump into hyperspace');
-        }
-    };
-    return MillenniumFalcon;
-}(Spacecraft));
-var falcon = new MillenniumFalcon();
+var falcon = new starfighters_1.MillenniumFalcon();
 falcon.jumpIntoHyperspace();
 var goodForTheJob = function (ship) { return ship.cargoContainers > 2; };
 console.log("Is falcon good for the job? " + (goodForTheJob(falcon) ? 'YES' : 'NO'));
